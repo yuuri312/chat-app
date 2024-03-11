e7297321190f689b8c7c1fdc8a887be379e54b50
yuuri312 <yz5@outlook.jp>
2024-03-11 17:40:32 +0900

    roomsテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_11_061109) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_11_082410) do
+  create_table "rooms", charset: "utf8", collation: "utf8_hungarian_ci", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", default: "", null: false
