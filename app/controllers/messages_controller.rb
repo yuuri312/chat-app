9939362080dd87347145bf935ba24cbc2b1bfc91
yuuri312 <yz5@outlook.jp>
2024-03-13 14:33:43 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,10 +6,12 @@ class MessagesController < ApplicationController
  
    def create
      @room = Room.find(params[:room_id])
+     @messages = @room.messages.includes(:user)
      @message = @room.messages.new(message_params)
      if @message.save
        redirect_to room_messages_path(@room)
      else
+      @messages = @room.messages.includes(:user)
        render :index, status: :unprocessable_entity
      end
    end
